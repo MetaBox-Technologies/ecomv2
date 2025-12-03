22c6c0de1f3ed2d6032233089fb6b1e41162f24d
khodadeenyoonusm <khodadeenyoonusm@gmail.com>
2025-12-03 16:18:37 +0400

    Services Ok

--- ecomv2-frontend/app/_componnents/HeroSection/swiper.tsx
+++ ecomv2-frontend/app/_componnents/HeroSection/swiper.tsx
@@ -17,25 +17,23 @@ export default function MySwiper() {
       spaceBetween={20}
       slidesPerView={1}
     >
-        <div className="herosection">
-      <SwiperSlide>
-        <div className="h-[737px] bg-blue-500 flex items-center justify-center text-white text-2xl">
+      <SwiperSlide className="slider">
+        <div className="h-[737px] bg-blue-500 flex pb-[100px]">
           Slide 1
         </div>
       </SwiperSlide>
 
       <SwiperSlide>
-        <div className="h-[737px] bg-red-500 flex items-center justify-center text-white text-2xl">
+        <div className="h-[737px] bg-red-500 flex">
           Slide 2
         </div>
       </SwiperSlide>
 
       <SwiperSlide>
-        <div className="h-[737px] bg-green-500 flex items-center justify-center text-white text-2xl">
+        <div className="h-[737px] bg-green-500 flex">
           Slide 3
         </div>
       </SwiperSlide>
-      </div>
     </Swiper>
     
   );
