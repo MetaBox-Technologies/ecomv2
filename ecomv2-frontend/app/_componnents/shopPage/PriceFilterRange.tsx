a41f4f7a2ad4d668dffde52f2053f911a6247ea1
shuaibmetabox <shuaib.metabox@outlook.com>
2025-12-24 11:39:04 +0400

    MUR instead of $ for filter

--- ecomv2-frontend/app/_componnents/shopPage/PriceFilterRange.tsx
+++ ecomv2-frontend/app/_componnents/shopPage/PriceFilterRange.tsx
@@ -10,11 +10,11 @@ export type PriceRange = {
 
 const priceRanges: PriceRange[] = [
   { label: "All prices", min: 0,max: Infinity},
-  { label: "$0.00 - 99.99", min: 0, max: 99.99 },
-  { label: "$100.00 - 199.99", min: 100, max: 199.99 },
-  { label: "$200.00 - 299.99", min: 200, max: 299.99 },
-  { label: "$300.00 - 399.99", min: 300, max: 399.99 },
-  { label: "$400.00+", min: 400, max: Infinity }
+  { label: "MUR 0 - 19,999", min: 0, max: 19999 },
+  { label: "MUR 20,000 - 39,999", min: 20000, max: 39999 },
+  { label: "MUR 40,000 - 59,999", min: 40000, max: 59999 },
+  { label: "MUR 60,000 - 79,999", min: 60000, max: 79999 },
+  { label: "MUR 80,000+", min: 80000, max: Infinity }
 ];
 
 interface Props {
